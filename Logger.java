650d16056561fc52920360ba631e72db1a9d4b1c
Desiree Schunn <152741459+DesireeSchunn@users.noreply.github.com>
2023-12-16 20:35:18 -0600

    Add files via upload

---
+++ Logger.java
@@ -0,0 +1,16 @@
+package week5codingproject;
+
+
+
+	public interface Logger {
+		
+		
+		public static void main(String[] args) {
+		}
+		
+		
+		public void log(String str);
+		
+		public void error(String str);
+		
+	}
